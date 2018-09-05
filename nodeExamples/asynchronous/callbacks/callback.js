2d896b981d6b7c0b90b0e938da7f7f0091fb2fcc
a1000891 <Ru$43876>
2018-09-05 15:50:19 +0530

    ASynchronous - callback

--- nodeExamples/asynchronous/callbacks/callback.js
+++ nodeExamples/asynchronous/callbacks/callback.js
@@ -3,9 +3,13 @@ console.log('Start Program');
 getUser(101, (user) => {
 	console.log('User - ', user);
 	getRepository(user.userName, (repos) => {
-		console.log('Repos - ', repos)
+		console.log('Repos - ', repos);
+		getCommits(repos[0], (commits) => {
+			console.log('Commits - ', commits);
+		});
 	});
 });
+//	Callback Hells - Nested Callback Functions or Christmas Tree Problem.
 
 function getUser(userId, callback) {
 	setTimeout(() => {
@@ -18,6 +22,13 @@ function getRepository(userName, callback) {
 	setTimeout(() => {
 		console.log('Calling Github for Repository.');
 		callback(['repo1', 'repo2', 'repo3']);
+	}, 2000);
+}
+
+function getCommits(repo, callback) {
+	setTimeout(() => {
+		console.log('Calling Github for Commits.');
+		callback(['commit1', 'commit2', 'commit3']);
 	}, 2000);
 }
 
