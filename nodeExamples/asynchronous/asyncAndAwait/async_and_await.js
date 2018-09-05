e6434ba0aa2d7cbbcb71f2692a9c6fcb9d1768c9
a1000891 <Ru$43876>
2018-09-05 17:23:42 +0530

    ASynchronous - ASync Await

---
+++ nodeExamples/asynchronous/asyncAndAwait/async_and_await.js
@@ -0,0 +1,43 @@
+console.log('Start Program');
+
+async function displayCommits() {
+	const user = await getUser(101);
+	console.log('User - ', user);
+
+	const repos = await getRepository(user.userName);
+	console.log('Repository - ', repos);
+
+	const commits = await getCommits(repos[0]);
+	console.log('Commits - ', commits);
+}
+
+displayCommits();
+
+function getUser(userId) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Retrieving User');
+			resolve({userId: userId, userName: 'Shobhit'});
+		}, 2000);
+	}); 
+}
+
+function getRepository(userName) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Getting Github Repository');
+			resolve(['repo1', 'repo2', 'repo3']);
+		}, 2000);
+	});
+}
+
+function getCommits(repo, callback) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('Getting Repository Commits');
+			resolve(['commit1', 'commit2', 'commit3']);
+		}, 2000);
+	});
+}
+
+console.log('End Program');
