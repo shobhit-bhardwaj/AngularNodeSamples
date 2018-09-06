aa63095ec6d43d1b7930ea59a966bafb0999526e
a1000891 <Ru$43876>
2018-09-06 20:23:26 +0530

    Mongoose - Connection And CRUD

--- nodeExamples/mongoose/connect_mongodb.js
+++ nodeExamples/mongoose/connect_mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://l4dvidap6393/test_db')
+mongoose.connect('mongodb://localhost/test_db')
 	.then((result) => console.log('MongoDB Connection Successfull'))
 	.catch((error) => console.log('Error in Connection - ', error));
