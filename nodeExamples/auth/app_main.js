a2fe9b4af4a61fc216f38f8c9830ec33c970620a
a1000891 <Ru$43876>
2018-09-18 18:21:45 +0530

    Change MongoDB Details

--- nodeExamples/auth/app_main.js
+++ nodeExamples/auth/app_main.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://l4dvidap6393/test_db')
+mongoose.connect('mongodb://localhost/test_db')
 	.then((result) => console.log('MongoDB Connection Successfull'))
 	.catch((error) => console.log('Error in Connection - ', error));
 
